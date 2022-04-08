826c52dfc98cbbad501211f0e3bc845569836ab8
psi <cy.lonsido@gmail.com>
2022-04-08 12:19:47 +0800

    helpers for normalizing request states on a particular resource

---
+++ src/utils/state.ts
@@ -0,0 +1,50 @@
+import Request from '@/types/request'
+import { AxiosPromise, AxiosResponse } from 'axios'
+
+
+/**
+ * Generic type for a callback fn.
+ * @param params - can be any, see `src/api/users` for reference.
+ * @returns AxiosPromise 
+ */
+
+type CallBackFn = <T>(params: T) => AxiosPromise
+
+
+/**
+ * Normalizes success or error state of a 
+ * requested resource. 
+ *
+ * @param callback - the axios request api for
+ * a particular resource.
+ * @param params - the callback parameter(s)
+ * @param message - custom message to pass on success.
+ *
+ * @returns the normalized request state
+ */
+
+export const GetNormalizedRequestState = 
+  async <T = Request.Success | Request.Error>(
+    callback: CallBackFn,
+    params: unknown,
+    message=''
+  ): Promise<T> => {
+  const requestState = {} as T
+
+  try {
+    const { data } = await callback(params)
+    Object.assign(requestState, {
+      state: 'ok',
+      message: data.message ?? message,
+      data: data.data,
+    })
+  } catch(error) {
+    const err = error as AxiosResponse
+    Object.assign(requestState, {
+      state: 'error',
+      message: err.data.message,
+      data: err.data.data,
+    })
+  }
+  return requestState
+}
