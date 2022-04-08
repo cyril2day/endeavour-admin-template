a8b679e6b2256940c0de21278326cac2a75ce94a
psi <cy.lonsido@gmail.com>
2022-04-08 12:19:57 +0800

    update request types

--- src/types/request/index.d.ts
+++ src/types/request/index.d.ts
@@ -2,20 +2,45 @@ import User from '@/types/user'
 
 export as namespace Request
 
+
+/**
+ * User info request success data without
+ * the password, last_login field
+ */
+
 type UserData = User.Bio &
   Omit<User.Account, 'password' | 'last_login'> &
   User.Permission
 
+
+/**
+ * For use in Roles list.
+ * TODO: refine this
+ */
+
 type RoleData = Object
 
-export type LoginData = {
+
+/**
+ * User store login request success data
+ */
+type LoginData = {
   mfa_verified: boolean
   otp_sent_to: string
   token: string
   verify: boolean
 }
 
+/**
+ * Union of possible request success data
+ */
+
 type RequestSuccessData = UserData | RoleData | LoginData
+
+
+/**
+ * Request error data is dynamic, depends on erring fields
+ */
 
 type RequestErrorData = {
   [field: string]: string[]
