378fb70801da1b43e4e170d0113d69c1f58164b1
psi <cy.lonsido@gmail.com>
2022-04-09 14:01:19 +0800

    update quasar config

--- quasar.config.js
+++ quasar.config.js
@@ -92,6 +92,7 @@ module.exports = configure(function (ctx) {
           layouts: path.join(__dirname, 'src/layouts'),
           assets: path.join(__dirname, 'src/assets'),
           stores: path.join(__dirname, 'src/stores'),
+          css: path.join(__dirname, 'src/css'),
         }
       },
       vitePlugins: [
@@ -108,17 +109,6 @@ module.exports = configure(function (ctx) {
             dts: './src/auto-imports.d.ts',
           },
         ],
-        /*
-        [
-          'vite-plugin-top-level-await',
-          {
-            // The export name of top-level await promise for each chunk module
-            promiseExportName: "__tla",
-            // The function to generate import names of top-level await promise in each chunk module
-            promiseImportName: i => `__tla_${i}`
-          }
-        ],
-        */
       ],
     },
 
