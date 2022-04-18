eb0400bf29b7a1166ff5e69e83a4c7f7dda92de9
psi <cy.lonsido@gmail.com>
2022-04-18 12:08:02 +0800

    update login index test

--- src/pages/auth/login/__test__/LoginIndex.spec.ts
+++ src/pages/auth/login/__test__/LoginIndex.spec.ts
@@ -1,5 +1,5 @@
 import useUserStore from '@/stores/user'
-import { mount, VueWrapper } from '@vue/test-utils'
+import { flushPromises, mount, VueWrapper } from '@vue/test-utils'
 import { mergeWith } from 'lodash'
 import LoginIndex from '../LoginIndex.vue'
 import {
@@ -8,6 +8,8 @@ import {
   TestMountingOptions,
 } from 'app/vitest/test-hooks'
 import { createTestingPinia } from '@pinia/testing'
+import { useRouter } from 'vue-router'
+import RequestState from '@/types/request'
 
 installQuasarPlugin()
 
@@ -74,6 +76,25 @@ describe('Login Index Component Test', () => {
     expect(password.props().modelValue).toBe(credentials.password)
   })
 
+  test('showPwd button works correctly', async () => {
+    expect.assertions(4)
+    const password = wrapper.findComponent({ ref: 'loginPassword' })
+    const showPwdBtn = wrapper.findComponent({ ref: 'showPwdBtn' })
+    const focusPwd = vi.spyOn(password.find('input').element, 'focus')
+
+    await showPwdBtn.trigger('click')
+    // input type is now text, password is shown
+    expect(password.props().type).toBe('text')
+    // focus on password input
+    expect(focusPwd).toHaveBeenCalledTimes(1)
+
+    await showPwdBtn.trigger('click')
+    // password is now hidden
+    expect(password.props().type).toBe('password')
+    // focus on password input
+    expect(focusPwd).toHaveBeenCalledTimes(2)
+  })
+
   test('login submit button is present', () => {
     expect.assertions(3)
     const submit = wrapper.findComponent({ ref: 'submitBtn' })
@@ -83,19 +104,95 @@ describe('Login Index Component Test', () => {
     expect(submit.props().iconRight).toBe('arrow_right_alt')
   })
 
-  test('login submit', async () => {
-    expect.assertions(3)
-    const login = vi.spyOn(userStore, 'Login')
-    const username = wrapper.findComponent({ ref: 'loginUsername' })
-    const password = wrapper.findComponent({ ref: 'loginPassword' })
+  test('login submit ok on user with mfa enabled', async () => {
+    expect.assertions(5)
+    const router = useRouter()
+    const loginOk = {
+      state: 'ok',
+      message: 'User Login Success',
+      data: { token: 'token', verify: true }, // user has mfa enabled
+    } as RequestState.Success
+    const login = vi.spyOn(userStore, 'Login').mockResolvedValue(loginOk)
     const submit = wrapper.findComponent({ ref: 'submitBtn' })
 
-    await username.setValue(credentials.username)
-    await password.setValue(credentials.password)
     await submit.trigger('click')
 
-    expect(submit.props().loading).toBe(true)
-    expect(login).toHaveBeenCalledTimes(1)
-    expect(login).toHaveBeenCalledWith(credentials)
+    expect(submit.props().loading).toBe(true) // disable button on click
+    expect(login).toHaveBeenCalledTimes(1) // call login api request
+    expect(router.push).toHaveBeenCalledTimes(1)
+    expect(router.push).toHaveBeenCalledWith({
+      name: 'Checkpoint',
+      params: { token: 'token' },
+    })
+
+    // submit button is enabled after successful login call
+    await nextTick()
+    expect(submit.props().loading).toBe(false)
   })
+
+  test('login submit ok on user without mfa enabled', async () => {
+    const router = useRouter()
+    const loginOk = {
+      state: 'ok',
+      message: 'User Login Success',
+      data: { token: 'token', verify: false }, // user has no mfa enabled
+    } as RequestState.Success
+    const login = vi.spyOn(userStore, 'Login').mockResolvedValue(loginOk)
+    const submit = wrapper.findComponent({ ref: 'submitBtn' })
+
+    await submit.trigger('click')
+    expect(submit.props().loading).toBe(true) // disable button on click
+    expect(login).toHaveBeenCalledTimes(1) // call login api request
+    expect(router.push).toHaveBeenCalledTimes(1)
+    expect(router.push).toHaveBeenCalledWith('/')
+
+    // submit button is enabled after successful login call
+    await nextTick()
+    expect(submit.props().loading).toBe(false)
+  })
+
+  test('failed login displays error feedbacks', async () => {
+    expect.assertions(3)
+    const loginFail = {
+      state: 'error',
+      message: 'Username or Password is Incorrect',
+    } as RequestState.Error
+    const login = vi.spyOn(userStore, 'Login').mockResolvedValue(loginFail)
+    const submit = wrapper.findComponent({ ref: 'submitBtn' })
+
+    await submit.trigger('click')
+    await flushPromises()
+
+    const feedback = wrapper.find('.login__feedback')
+
+    expect(login).toHaveBeenCalledTimes(1) // call login api request
+    expect(feedback.html()).toContain('Username or Password is Incorrect')
+
+    // submit button is enabled after the login call
+    await nextTick()
+    expect(submit.props().loading).toBe(false)
+  })
+
+  test('redirects to the MFA page if login token is set in the device storage (Cookies)', async () => {
+    document.cookie = 'login_token=token'
+    createWrapper()
+    await nextTick()
+    const router = useRouter()
+
+    expect(router.push).toHaveBeenCalledTimes(1)
+  })
+
+  /*
+  test(
+    'redirects to the MFA page if login token is set in the device storage (LocalStorage)',
+    async() => {
+      window.localStorage.setItem('login_token', 'token')
+      createWrapper()
+      await nextTick()
+      const router = useRouter()
+
+      expect(router.push).toHaveBeenCalledTimes(1)
+    }
+  )
+  */
 })
